1c4bc1e767bd0c55637ab2406b5dcb61001bd47e
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-09-27 16:35:30 +0200

    first Spring application 2

--- src/main/java/pl/sda/jpajava5/JpaJava5Application.java
+++ src/main/java/pl/sda/jpajava5/JpaJava5Application.java
@@ -1,5 +1,7 @@
 package pl.sda.jpajava5;
-
+/*
+* started Ultimate version of Intellij
+ */
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
